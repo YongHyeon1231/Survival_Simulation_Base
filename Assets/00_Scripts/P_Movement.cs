b51cc4ec4eeee269d8150a040b12ed4a970325a3
PYH <dydgustmdfl@gmail.com>
2026-03-25 18:36:12 +0900

    [add] 캐릭터 이동
    간단한 캐릭터 이동 구현
    
    *forward는 Inspector에 따로 있는 값이 아니라, Rotation으로부터 계산되는 방향 벡터다

--- Assets/00_Scripts/P_Movement.cs
+++ Assets/00_Scripts/P_Movement.cs
@@ -6,5 +6,34 @@ public class P_Movement : MonoBehaviour
 {
     [Header("#Movement Settings")]
     public float moveSpeed = 5.0f;
-    public float gravity = -9.81f;
+
+    private CharacterController controller;
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal"); //A/D or Left/Right
+        float vertical = Input.GetAxis("Vertical"); //W/S or Up/Down
+
+        Vector3 cameraForward = Camera.main.transform.forward; //forward는 Inspector에 따로 있는 값이 아니라, Rotation으로부터 계산되는 방향 벡터다
+        Vector3 cameraRight = Camera.main.transform.right;
+
+        cameraForward.y = 0.0f;
+        cameraRight.y = 0.0f;
+
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        Vector3 moveDirection = cameraForward * vertical + cameraRight * horizontal;
+
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
 }
