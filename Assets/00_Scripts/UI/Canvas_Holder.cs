606f55042c5f9d35c289042e2a817ff99a02ea51
PYH <dydgustmdfl@gmail.com>
2026-04-03 17:28:51 +0900

    [add] 플레이어와 오브젝트 상호작용에 따른 이벤트 추가
    F키 입력시 오브젝트 공격 모션

--- Assets/00_Scripts/UI/Canvas_Holder.cs
+++ Assets/00_Scripts/UI/Canvas_Holder.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Canvas_Holder : MonoBehaviour
 {
+    public static Canvas_Holder instance = null;
+
     [SerializeField] private GameObject Board;
+    [SerializeField] private Image BoardHpFill, BoardHpWhiteFill;
+    Coroutine F_Coroutine;
+
+    private void Awake()
+    {
+        if(instance == null) instance = this;
+    }
 
     private void Start()
     {
@@ -14,7 +25,34 @@ public class Canvas_Holder : MonoBehaviour
     public void GetBoard()
     {
         Board.SetActive(true);
+        BoardHpFill.fillAmount = 1.0f;
+        BoardHpWhiteFill.fillAmount = 1.0f;
     }
 
     public void BoardOut() => Board.GetComponent<UI_Animation_Handler>().AnimationChange("Out");
+
+    public void BoardFill(float hp, float maxHp)
+    {
+        BoardHpFill.fillAmount = hp / maxHp;
+        if (F_Coroutine != null) // 기존 애니메이션 중지 후 새로 시작
+        {
+            StopCoroutine(F_Coroutine);
+        }
+        F_Coroutine = StartCoroutine(FillCoroutine());
+    }
+
+    IEnumerator FillCoroutine()
+    {
+        while(BoardHpWhiteFill.fillAmount - BoardHpFill.fillAmount > 0.001f)
+        {
+            BoardHpWhiteFill.fillAmount = 
+            Mathf.Lerp(BoardHpWhiteFill.fillAmount,
+                        BoardHpFill.fillAmount, 
+                        Time.deltaTime * 2.0f);
+
+            yield return null;
+        }
+
+        BoardHpWhiteFill.fillAmount = BoardHpFill.fillAmount; // 최종적으로 정확히 일치하도록 설정
+    }
 }
